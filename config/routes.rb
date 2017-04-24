7ef2ec0cecc2b851b8b62f09e6ff51b9c72a6c5b
Trinidad Araujo <trinidadandeli@gmail.com>
2017-04-24 09:14:15 -0700

    First commit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  root 'hikes#index'
+  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
+  get '/login', to: 'sessions#new'
+  get '/logout', to: 'sessions#destroy'
+end
