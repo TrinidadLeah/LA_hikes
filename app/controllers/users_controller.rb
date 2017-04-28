dedf3bc33a916bfa6255b3ad34c73ceb3b0798bc
Trinidad Araujo <trinidadandeli@gmail.com>
2017-04-28 10:06:25 -0700

    fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,8 @@ end
 
 def create@ user = User.new(user_params)
 if@ user.save
-session[: user_id] = @user.id
-flash[: notice] = "You have successfully signed up!"
+session[:user_id] = @user.id
+flash[:notice] = "You have successfully signed up!"
 redirect_to root_path
 else
     render: new
@@ -15,6 +15,6 @@ private
 
 
 def user_params
-params.require(: user).permit(: name, : email, : password, : password_confirmation)
+params.require(:user).permit(:name, :email, :password, :password_confirmation)
 end
 end
